7e754dd57a4d56b4147335cbedbd4a647418d0d6
marius koudou <koudousaintmarius@gmail.com>
2016-09-13 21:54:31 +0000

    remove passport in express middleware

--- api/controllers/messengerController.js
+++ api/controllers/messengerController.js
@@ -175,4 +175,43 @@ exports.webhookpost = function (req, res) {
 
         callSendAPI(messageData);
     }
+
+    function receivedDeliveryConfirmation(event) {
+        var senderID = event.sender.id;
+        var recipientID = event.recipient.id;
+        var delivery = event.delivery;
+        var messageIDs = delivery.mids;
+        var watermark = delivery.watermark;
+        var sequenceNumber = delivery.seq;
+
+        if (messageIDs) {
+            messageIDs.forEach(function(messageID) {
+                console.log("Received delivery confirmation for message ID: %s",
+                    messageID);
+            });
+        }
+
+        console.log("All message before %d were delivered.", watermark);
+    }
+
+    function receivedAuthentication(event) {
+        var senderID = event.sender.id;
+        var recipientID = event.recipient.id;
+        var timeOfAuth = event.timestamp;
+
+        // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
+        // The developer can set this to an arbitrary value to associate the
+        // authentication callback with the 'Send to Messenger' click event. This is
+        // a way to do account linking when the user clicks the 'Send to Messenger'
+        // plugin.
+        var passThroughParam = event.optin.ref;
+
+        console.log("Received authentication for user %d and page %d with pass " +
+            "through param '%s' at %d", senderID, recipientID, passThroughParam,
+            timeOfAuth);
+
+        // When an authentication is received, we'll send a message back to the sender
+        // to let them know it was successful.
+        sendTextMessage(senderID, "Authentication successful");
+    }
 }

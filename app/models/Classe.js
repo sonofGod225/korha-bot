39e10e9e3b3b01848015e49b13e008434161cae1
marius koudou <koudousaintmarius@gmail.com>
2016-09-16 02:26:32 +0000

    add class classroom

--- app/models/Classe.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var classSchema = new Schema({
-    name: { type: String, default: '' },
-    commentaireBot: { type: String, default: '' }
-});
-
-module.exports = mongoose.model('Classe',classSchema);

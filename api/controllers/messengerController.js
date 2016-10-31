afc2c49bf1c444a4eb34f043a2b7ffb4bb801c28
marius koudou <marius koudou>
2016-10-31 13:34:28 +0000

    check if lesson as video

--- api/controllers/messengerController.js
+++ api/controllers/messengerController.js
@@ -493,7 +493,7 @@ function sendButtonMessageWithLesson(recipientId, gradeid, courseid, chapterid, 
                     }
                 }).then(function (quiz) {
 
-                    if (lessonVideo !== 'null') {
+                    if (lessonVideo != '') {
                         console.log("video link_" + lessonVideo);
                         let buttonLessonVideo = {
                             type: "postback",

f1e2b2b2ae0863875778647cd63986b24cea65f4
marius koudou <koudousaintmarius@gmail.com>
2017-01-14 00:55:35 +0000

    add facebook  app key

--- config/env/production.js
+++ config/env/production.js
@@ -33,7 +33,7 @@ module.exports = {
         ]
     },
     facebookmessenger: {
-        appSecret:"6d741afeda0b64e47c43480b4b3d9073",
+        appSecret:"bf36d7f0aa2fdbb72ea93c2f8aa66266",
         pageAccessToken: "EAAaowVJGpCoBAKMZBkG86s34hbTpe5F6wpqHlfCaDq2FybyMu28zVm0MmZB5JZBxyLcZC8n4xKHM2hDVZBq6ZBjzVajGvj63mB3vQGBZCVQnO4w17p7yeDzdT9Yst7ogA5lsZColw55AR63h0Kj0mUEefeprXbn97TwwxGdk56NSmQZDZD",
         validationToken: "mon_token_de_verification",
         serverURL: "/",
